c453c82279d1bc5e382a0dd186aea8678f0d4db9
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2020-04-10 00:24:54 +0530

    Update substrate version after a release

--- .ci/release.sh
+++ .ci/release.sh
@@ -12,5 +12,12 @@ newVersion=${TRAVIS_TAG%.*}.$((${TRAVIS_TAG##*.} + 1))
 # version 'newVersion-SNAPSHOT'
 sed -i -z "0,/\nversion '$TRAVIS_TAG'/s//\nversion '$newVersion-SNAPSHOT'/" build.gradle
 
+# Find substrate version
+substrateVersion=$(grep com.gluonhq:substrate: build.gradle | tr -d "'" | cut -d \: -f 3)
+# Update version by 1
+newSubstrateVersion=${substrateVersion%.*}.$((${substrateVersion##*.} + 1))
+# Update Substrate version
+sed -i -z "0,/com.gluonhq:substrate:$substrateVersion/s//com.gluonhq:substrate:$newSubstrateVersion-SNAPSHOT/" build.gradle
+
 git commit build.gradle -m "Upgrade version to $newVersion-SNAPSHOT" --author "Github Bot <githubbot@gluonhq.com>"
 git push https://gluon-bot:$GITHUB_PASSWORD@github.com/gluonhq/client-gradle-plugin HEAD:master
