f9d91e88fccb1eebaa15bc5d6a1cd6c66a1a4f5f
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2021-02-17 19:06:00 +0530

    Install AWS CLI before executing aws command

--- .ci/release.sh
+++ .ci/release.sh
@@ -10,7 +10,7 @@ git config --global user.email "githubbot@gluonhq.com"
 # Update version by 1
 newVersion=${TRAVIS_TAG%.*}.$((${TRAVIS_TAG##*.} + 1))
 
-# Update README with the latest released version
+echo "Update README with the latest released version"
 sed -i "0,/id 'com.gluonhq.client-gradle-plugin' version '.*'/s//id 'com.gluonhq.client-gradle-plugin' version '$TRAVIS_TAG'/" README.md
 sed -i "0,/'com.gluonhq:client-gradle-plugin:.*'/s//'com.gluonhq:client-gradle-plugin:$TRAVIS_TAG'/" README.md
 git commit README.md -m "Use latest release v$TRAVIS_TAG in README"
@@ -25,7 +25,7 @@ sed -i -z "0,/\nversion '$TRAVIS_TAG'/s//\nversion '$newVersion-SNAPSHOT'/" buil
 substrateVersion=$(grep com.gluonhq:substrate: build.gradle | tr -d "'" | cut -d \: -f 3)
 # Update version by 1
 newSubstrateVersion=${substrateVersion%.*}.$((${substrateVersion##*.} + 1))
-# Update Substrate version
+echo "Update Substrate version"
 sed -i -z "0,/com.gluonhq:substrate:$substrateVersion/s//com.gluonhq:substrate:$newSubstrateVersion-SNAPSHOT/" build.gradle
 
 git commit build.gradle -m "Prepare development of $newVersion"
